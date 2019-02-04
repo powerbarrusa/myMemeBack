585aa7e7951a8027335569da60141e360250006c
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-02-04 15:47:58 -0700

    added dotenv

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 var cors = require('cors')
+const dotenv = require("dotenv").config()
 
 app.use(cors())
 
