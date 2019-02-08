cf6a560675f8b3fcdd99f27fa2183d9440a720ac
Cesar Montoya <cesarm@Cesars-MacBook-Pro.local>
2019-02-08 14:27:37 -0700

    change

--- seeds/memes.js
+++ seeds/memes.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('memes').insert([
         {id: 1, image_url: "https://i.imgur.com/1PCyDuD.jpg", top_text: "V1 I'm on top!", bottom_text: "V1 I'm on bottom!"},
-        {id: 2, image_url: "http://memebomb.net/wp-content/uploads/2018/10/blank-meme-maker-2.jpg", top_text: "V3 I'm on top!", bottom_text: "V3 I'm on bottom!"},
+        {id: 2, image_url: "http://memebomb.net/wp-content/uploads/2018/10/blank-meme-maker-2.jpg", top_text: "That feeling when", bottom_text: "you decide to use materialize"},
         {id: 3, image_url: "https://comicsandmemes.com/wp-content/uploads/blank-meme-template-018-office-space-if-you-could-just.png", top_text: "If Heroku would just deploy my site", bottom_text: "that would be great"}
       ])
     })
@@ -16,4 +16,3 @@ exports.seed = function(knex, Promise) {
       )
     })
 }
-
